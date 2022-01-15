326b1cc870b757a2a327e668f7a7156f5b8fb5c4
Mike Gajewski <mike-gajewski@idleblink.org>
2022-01-14 19:06:04 -0500

    Change database from SQLite to PosgreSQL, including install psycopg2-binary and add it to requirements.txt.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import flask
 from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_bootstrap import Bootstrap
@@ -12,12 +13,14 @@ from flask_gravatar import Gravatar
 from functools import wraps
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
